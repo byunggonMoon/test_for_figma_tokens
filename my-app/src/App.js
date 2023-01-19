7cff933bc0aa218674bb81e6cca4ac379089462b
byunggonMoon <121859124+byunggonMoon@users.noreply.github.com>
2023-01-19 14:52:13 +0900

    add : edit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import output from "./output.json"
 function App() {
+
+console.log(output)
+
   return (
     <div className="App">
       <header className="App-header">
